0a315f9ae0ea16b1e0f9fcfaeb7bba059a2dbebd
Dean Howe <hideanhowe@gmail.com>
2025-11-20 21:28:43 +0000

    mess

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
 // Two fingerprints are considered a match if:
 // They are the same length.
 // The number of differing characters does not exceed 10% of the fingerprint length.
+
+function isMatch(fingerprintA, fingerprintB) {
+  // check the length
+  console.log(fingerprintA, fingerprintB);
+  if (fingerprintA.length === fingerprintB.length) {
+    console.log('so far so good');
+    let length = fingerprintA.match(fingerprintB);
+    console.log(length);
+    if ((fingerprintA.length / 100) * 10 >= length.length) {
+      console.log('We have a match');
+      return true;
+    }
+    //console.log(fingerprintA.length / 100 * 10)
+  }
+  return false;
+}
+
+// isMatch("helloworld", "jelloworld")
