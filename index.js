3b34d16cac899e931e5c47564f0756435c8192f5
Dean Howe <hideanhowe@gmail.com>
2025-11-28 06:59:56 +0000

    success mess

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function isMatch(fingerprintA, fingerprintB) {
     //  console.log(fingerprintA, fingerprintB)
     if (fingerprintA.length === fingerprintB.length) {
       console.log('so far so good');
+
       // now maybe convert to array or set...?
       // set finds unique characters, if there are more than 10% of a.length then fail..?
       const lettersA = new Set();
@@ -23,42 +24,61 @@ function isMatch(fingerprintA, fingerprintB) {
       // convert to arrays
       const arrayA = fingerprintA.split('');
       const arrayB = fingerprintB.split('');
-      console.log(arrayA.length, arrayB.length);
+
+      // use another technique where a counter is created to count the different array entries when compared
+      // console.log(arrayA, arrayB)
+
+      function compareArrays(a, b) {
+        let counter = [];
+        for (let i = 1; i <= a.length; i++) {
+          if (a[i] !== b[i]) counter.push(b[i]);
+        }
+        return counter;
+      }
+
+      const filtered = compareArrays(arrayA, arrayB);
+      console.log('This is the filtered array: ', filtered, filtered.length);
+
+      // check filtered.length is less than 10% of array.length
+      return filtered.length <= (arrayA.length / 100) * 10;
+
+      // console.log(arrayA.length, arrayB.length)
       // add the prints to set
-      arrayA.map((x) => lettersA.add(x));
+      // arrayA.map((x) => lettersA.add(x))
       // check the size of the set
-      console.log(lettersA.size);
-      arrayB.map((x) => lettersB.add(x));
+      // console.log("Letters A: ", lettersA.size)
+      // arrayB.map((x) => lettersB.add(x))
       // check the size of the set
-      console.log(lettersB.size);
-      arrayA.map((x) => lettersC.add(x));
-      arrayB.map((x) => lettersC.add(x));
+      // console.log("Letters B: ", lettersB.size)
+      // arrayA.map((x) => lettersC.add(x))
+      // arrayB.map((x) => lettersC.add(x))
       // check the size of the set
-      console.log(lettersC.size);
+      // console.log("Letters C: ", lettersC.size)
       // check how many different letters there are
-      const test = lettersA.difference(lettersB);
-      console.log(lettersC.difference(lettersA));
-      console.log(lettersC.difference(lettersB));
-      console.log(test);
-      console.log(test.size);
+      // const test = lettersA.difference(lettersB)
+      // console.log(lettersC.difference(lettersA))
+      // console.log(lettersC.difference(lettersB))
+      // console.log(test)
+      // console.log(test.size)
       // check if test.size is less than 10% of fingerprintA.length
       // console.log("true")
-      console.log(typeof test.size);
-      const percent = (fingerprintB.length / 100) * 10;
-      console.log('print size: ', percent);
-      if (test.size <= percent) {
-        return true;
-      } else {
-        return false;
-      }
-      // return test.size  <= percent;
-      //
-      // let length = fingerprintA.match(fingerprintB)
-      //console.log(length)
-      // if (fingerprintA.length / 100 * 10 >= length.length) {
-      // console.log("We have a match")
-      // return true
+      // console.log(typeof test.size)
+      // const percent = fingerprintB.length / 100 * 10;
+      // console.log("print size: ", percent)
+      // if (test.size <= percent) {
+      //   return true
+      // } else {
+      //   return false
     }
+    return false;
+    // return test.size  <= percent;
+    //
+    // let length = fingerprintA.match(fingerprintB)
+    //console.log(length)
+    // if (fingerprintA.length / 100 * 10 >= length.length) {
+    // console.log("We have a match")
+    // return true
+    // }
     //console.log(fingerprintA.length / 100 * 10)
     //return false
   }
@@ -69,6 +89,28 @@ function isMatch(fingerprintA, fingerprintB) {
 
 // Create a Set
 // const letters = new Set();
+
+// const compareArrays = (a, b) => {
+//   if (a.length !== b.length) return false;
+//   else {
+//     // Comparing each element of your array
+//     for (var i = 0; i < a.length; i++) {
+//       if (a[i] !== b[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
+
+// let array1 = [21, null, 33];
+// let array2 = [21, 22, 23];
+// let array3 = [21, undefined, 33];
+// let array4 = [21, 22, 23];
+
+// console.log(compareArrays(array1, array2)); //false
+// console.log(compareArrays(array1, array3)); //false
+// console.log(compareArrays(array2, array4)); //true
 
 isMatch(
   'thequickbrownfoxjumpsoverthelazydog',
